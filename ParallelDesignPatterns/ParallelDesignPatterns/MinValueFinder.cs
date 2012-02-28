fb3ff6cfe6f68b1eef1110b60e11a140e2d9a9ee
balopat <balopat@balopat.com>
2012-02-28 08:48:29 +0000

    cleanup

---
+++ ParallelDesignPatterns/ParallelDesignPatterns/MinValueFinder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ParallelDesignPatterns
+{
+    class MinValueFinder
+    {
+        private Func<int, int> f;
+
+        public MinValueFinder(Func<int, int> f)
+        {
+            this.f = f;
+        }
+
+        internal int findMinimum(int a, int b)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ParallelDesignPatterns/ParallelDesignPatterns/MinValueFinderTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Threading;
+
+namespace ParallelDesignPatterns
+{
+    
+
+    [TestFixture]
+    class MinValueFinderTest
+    {
+        private Func<int, int>  sqr = x => x * x;
+        private Func<int, int> neg = x => -x;
+        private Func<int, int> slowNeg = x => { Thread.Sleep(200); return -x; };
+
+        [Test]
+        public void shouldReturnTheFunctionValueForOneElementLongInterval()
+        {
+            Assert.AreEqual(new MinValueFinder(sqr).findMinimum(-1, -1), 1);
+        }
+
+        [Test]
+        public void givenSqrFunctionAndIntervalFrom2To5ShouldResultIn4()
+        {
+            Assert.AreEqual(new MinValueFinder(sqr).findMinimum(2, 5), 4);
+        }
+
+        [Test]
+        public void givenSqrFunctionAndIntervalFromMinus4To10ShouldResultIn0()
+        {
+            Assert.AreEqual(new MinValueFinder(sqr).findMinimum(-4, 10), 0);
+        }
+
+        [Test]
+        public void givenNegFunctionAndIntervalFromMinus4To10ShouldResultInMinus10()
+        {
+            Assert.AreEqual(new MinValueFinder(neg).findMinimum(-4, 10), -10);
+        }
+
+        [Test, Timeout(1000)]
+        public void shouldFinishInTime()
+        {
+            Assert.AreEqual(new MinValueFinder(slowNeg).findMinimum(-4, 10), -10);
+        }
+    }
+}
+
+
